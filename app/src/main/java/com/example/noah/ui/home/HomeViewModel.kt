9d061365625959b540c19e5bdef4fb20787939a9
dlaywls <gywls5184@naver.com>
2023-07-28 11:41:21 +0900

    community_version1 데이터베이스 , 글쓰기 레이아웃 추가

--- app/src/main/java/com/example/noah/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/noah/ui/home/HomeViewModel.kt
@@ -1,20 +1,5 @@
 package com.example.noah.ui.home
 
-class HomeViewModel{
 
-    /*private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text*/
-    var id: String?=" "
-    var title:String=""
-    var contents:String=""
-    constructor(){}
 
-    constructor(id: String?, title: String, contents: String){
-        this.id = id
-        this.title = title
-        this.contents = contents
-    }
-
-}
+data class HomeViewModel(val id:Int, val title:String, val contents:String)
