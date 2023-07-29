af6f937017062ec7b16c58bef9cae86b86211e88
heojihyang <84635299+Heojihyang@users.noreply.github.com>
2023-07-29 13:53:56 +0900

    🖥️ Edit | 화면 이동
    첫 입장 시 로고 화면 2초 후
    
    1. 로그인o -> 메인 화면
    2. 로그인x -> 로그인 화면
    2-1. 로그인 성공 시 로그인 -> 메인 화면
    
    메인화면 이동은 어떤 Activity사용하는지 몰라서 비워뒀습니다!

--- app/src/main/java/com/example/noah/ui/home/whatToDo.kt
+++ app/src/main/java/com/example/noah/ui/home/whatToDo.kt
@@ -1,15 +1,18 @@
 package com.example.noah.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.noah.R
 
 
-class whatToDo : Fragment(R.layout.fragment_what_to_do) {
-
+class whatTodo : Fragment() {
 
 
 
@@ -17,12 +20,21 @@ class whatToDo : Fragment(R.layout.fragment_what_to_do) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_what_to_do, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_what_todo, container, false)
+    }
+    fun onClickEarthquake() {
+        Log.i("whatTodo", "onClickEarthquake")
+        Toast.makeText(context, "지진 발생 시", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickPortalSite() {
+        Log.i("whatTodo", "onClickPortalSite")
+        Toast.makeText(context, "국민 재난 안전 포털", Toast.LENGTH_SHORT).show()
+        //웹 페이지 열기 위한 intent
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.safekorea.go.kr/"))
+        startActivity(intent)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        super.onViewCreated(view, savedInstanceState)
-    }
+
 }
