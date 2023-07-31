8a5be60120d8c3f55fa568bb2be386399491abc3
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2023-08-01 02:44:21 +0900

    주소지 변경 데이터 넘기기 시도

--- app/src/main/java/com/example/noah/SearchRoadActivity.kt
+++ app/src/main/java/com/example/noah/SearchRoadActivity.kt
@@ -1,15 +1,17 @@
 package com.example.noah
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 class SearchRoadActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_road)
@@ -26,19 +28,21 @@ class SearchRoadActivity : AppCompatActivity() {
 
         //최초 웹뷰 로드
         webView.loadUrl("https://noah-46984.web.app")
+
     }
 
-    private class BridgeInterface {
-        lateinit var mContext: Context
+    class BridgeInterface {
 
         @JavascriptInterface
         fun processDATA(data: String) {
             //다음(카카오) 주소 검색 API의 결과 값이 브릿지 통로를 통해 전달
             val intent = Intent()
             intent.putExtra("data", data)
-            val activity: SetAddress = mContext as SetAddress
-            activity.setResult(Activity.RESULT_OK, intent)
-            activity.finish()
+            Log.d("dataMove", data)
+            (context as? Activity)?.run {
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
         }
     }
 
--- app/src/main/java/com/example/noah/SetAddress.kt
+++ app/src/main/java/com/example/noah/SetAddress.kt
@@ -2,6 +2,7 @@ package com.example.noah
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -52,8 +53,12 @@ class SetAddress : AppCompatActivity() {
     ) { result: ActivityResult ->
         //SearchRoad Activity로부터의 결과 값이 이곳으로 전달
         if (result.resultCode == RESULT_OK) {
+            Log.d("dataResult", "data 없음")
             if (result.data != null) {
+                Log.d("dataResult", "data 읽기")
                 val data = result.data!!.getStringExtra("data")
+                Log.d("dataResult", data.toString())
+//                val data = result.data?.getStringExtra("data")
                 mEtAddress.setText(data)
             }
         }
