c35935ed5d36193cac8374572346a64e7e265d5f
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2023-08-01 23:34:22 +0900

    main 다시 merge

--- app/src/main/java/com/example/noah/SetAddress.kt
+++ app/src/main/java/com/example/noah/SetAddress.kt
@@ -9,8 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class SetAddress : AppCompatActivity() {
-
-//    lateinit var getAddress: EditText // EditText 변수를 lateinit으로 선언
       lateinit var mEtAddress: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,23 +25,6 @@ class SetAddress : AppCompatActivity() {
             getSearchResult.launch(intent)
         }
 
-//        getAddress = findViewById(R.id.getAddress) // findViewById는 onCreate 내부에서 호출
-//
-//        getAddress.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//                // 입력하기 전에 호출되는 메서드
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                // 입력 중일 때 호출되는 메서드
-//                val text = s.toString()
-//                // 여기서 입력 중인 텍스트를 처리하면 됩니다.
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//                // 입력이 완료된 후에 호출되는 메서드
-//            }
-//        })
     }
 
     private val getSearchResult = registerForActivityResult<Intent, ActivityResult>(
